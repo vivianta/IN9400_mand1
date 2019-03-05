780bca75522498a6fd9e4e8ece87a8d3572ef527
chao <chao.mellow@gmail.com>
2019-03-05 13:26:08 +0100

    finish and deliver

--- cnn/conv_layers.py
+++ cnn/conv_layers.py
@@ -53,27 +53,37 @@ def conv_layer_forward(input_layer, weight, bias, pad_size=1, stride=1):
             for cc in range(channels_x):
                 x = input_layer[i,cc,:,:]
                 filter_j = weight[j,cc,:,:]
-                """    
-                out_cc= signal.convolve2d(x, filter_j, mode="same",boundary='fill', fillvalue=0)
-                out += out_cc
-                """
+
                 out_cc = np.zeros_like(out)
                 x_pad = np.pad(x,(K,K),'constant', constant_values=(0, 0))
-                for p in range(height_x, Std):
-                    for q in range(width_x, Std):
+
+                """
+                if(i==0 and j==0 and cc==0):
+                    print("filter_j,",filter_j)
+                    print("compute convolution....")
+                    print("x_pad,", x_pad)
+                """
+
+                for p in range(0,height_x, Std):
+                    for q in range(0,width_x, Std):
                         x_masked = x_pad[p:(p+height_w),q:(q+width_w)]
                         ypq = np.sum(x_masked*filter_j)
                         """
+                        ypq = 0
                         for r in range(-1*K,(K+1)):
                             for s in range(-1*K,(K+1)):
-                                ypq += x_pad[p+K+r,q+K+s]*filter_j[r+K,s+K]
-                                #ypq += x_pad[p+r,q+s]*filter_j[r,s]
+                                if((i+j+cc+p+q)==0 ):
+                                    print("i=",(1-r),", j=",(1-s),"filter[r,s]",filter_j[1-r,1-s])
+                                if((i+j+cc)==0):
+                                    print("i=",(p+K+r),", j=",(q+K+s),"x[r,s]",x_pad[p+K+r,q+K+s])
+                                ypq += x_pad[p+K+r,q+K+s]*filter_j[1-r,1-s]
                         """
-                        out_cc[p,q]=ypq
+                        pp = int(p/Std)
+                        qq = int(q/Std)
+
+                        out_cc[pp,qq]=ypq
                 out+=out_cc
-
-                output_layer[i,j,:,:] = bias[j]+out
-
+            output_layer[i,j,:,:] = bias[j]+out
     return output_layer
 
 
@@ -94,7 +104,9 @@ def conv_layer_backward(output_layer_gradient, input_layer, weight, bias, pad_si
         bias_gradient: Gradient of the loss L with respect to the biases b
     """
     # TODO: Task 2.2
-    input_layer_gradient, weight_gradient, bias_gradient = None, None, None
+    input_layer_gradient, weight_gradient, bias_gradient = np.zeros_like(input_layer),\
+                                                           np.zeros_like(weight),\
+                                                           np.zeros_like(bias)
 
     batch_size, channels_y, height_y, width_y = output_layer_gradient.shape
     batch_size, channels_x, height_x, width_x = input_layer.shape
@@ -104,6 +116,40 @@ def conv_layer_backward(output_layer_gradient, input_layer, weight, bias, pad_si
         "The number of filters must be the same as the number of output layer channels")
     assert channels_w == channels_x, (
         "The number of filter channels be the same as the number of input layer channels")
+
+    K = pad_size
+    Std=1
+    
+    #bias_gradient
+    bias_gradient = np.zeros_like(bias)
+    #for i in range(batch_size): not necessary because the gradients are also summed over samples
+    for j in range(num_filters):
+        bias_gradient[j] += np.sum(output_layer_gradient[:,j,:,:])
+
+    #weight_gradient
+    weight_gradient = np.zeros_like(weight)
+    for i in range(batch_size):
+        for j in range(num_filters):
+            for cc in range(channels_x):
+                x_pad = np.pad(input_layer[i,cc,:,:],(K,K),'constant', constant_values=(0, 0))
+                #x_pad = np.flip(np.pad(input_layer[i,cc,:,:],(K,K),'constant', constant_values=(0, 0)))
+                for r in range(height_w):
+                    for s in range(width_w):
+                        x_mask = x_pad[r:(r+height_y),s:(s+width_y)]
+                        weight_gradient[j,cc,r,s] += \
+                            np.sum(output_layer_gradient[i,j,:,:]*x_mask)
+
+    #input_layer_gradient
+    input_padded = np.pad(input_layer, ((0,), (0,), (K,), (K,)), mode="constant", constant_values=0)
+    input_gradient_padded = np.zeros_like(input_padded)
+
+    for i in range(batch_size):
+        for j in range(height_y):
+            for k in range(width_y):
+                input_gradient_padded[i, :, j*Std:j*Std+height_w, k*Std:k*Std+width_w] += \
+                np.sum((weight[:, :, :, :] * (output_layer_gradient[i, :, j, k])[:, None, None, None]), axis=0)
+
+    input_layer_gradient = input_gradient_padded[:, :, K:-K, K:-K]
 
     return input_layer_gradient, weight_gradient, bias_gradient
 
